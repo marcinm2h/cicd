3a765c8abb9b4d2fbe627be317e6356fea878585
Marcin Moch <marcio.moch@gmail.com>
2022-07-02 13:34:23 +0200

    render.com

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex-1">
-      <h1 className="text-center text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-center text-3xl font-bold">Hello world! (render.com?)</h1>
     </div>
   );
 }
