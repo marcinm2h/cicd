ba72e98afd87af673019b48c86a848f8f81f6378
Marcin Moch <marcio.moch@gmail.com>
2022-07-02 18:01:45 +0200

    tailwind test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <div className="flex flex-row justify-center">
+      <div className="p-2 bg-black text-white flex items-center text-3xl font-bold">
+        Hello
+      </div>
+      <div className="p-2 border-4 border-black  flex items-center text-3xl font-bold">
+        world!
+      </div>
+    </div>
+  );
 }
 
 export default App;
